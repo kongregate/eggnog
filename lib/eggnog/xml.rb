88287a3e87d776a92a77e2a09e3f9682446b6f04
Drew Chandler <drew@kongregate.com>
2013-11-06 17:23:46 -0800

    Fix too short escaped multibyte character on Ruby 2.0.0

--- lib/eggnog/xml.rb
+++ lib/eggnog/xml.rb
@@ -32,7 +32,7 @@ module Eggnog
         raise ArgumentError, "+xml+ must be instance of String"
       end
 
-      xml = xml.gsub(/([\302|\240|\s|\n|\t])|(\&nbsp;?){1,}/, ' ').strip
+      xml = xml.gsub(/([\u00A0|\s|\n|\t])|(\&nbsp;?){1,}/, ' ').strip
 
       unless xml.length > 0
         return {}
